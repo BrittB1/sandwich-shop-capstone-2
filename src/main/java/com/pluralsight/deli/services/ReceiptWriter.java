5ac9ea25f1f5a4f4c3c8af6ea7c1e2c562633a12
Brittany Buchanan <bbuchanan0400@gmail.com>
2025-11-12 20:51:22 -0500

    Created method in ReceiptWriter to save receipt and handle errors.

--- src/main/java/com/pluralsight/deli/services/ReceiptWriter.java
+++ src/main/java/com/pluralsight/deli/services/ReceiptWriter.java
@@ -1,6 +1,11 @@
 package com.pluralsight.deli.services;
 
+import com.pluralsight.deli.models.Order;
+
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -24,8 +29,30 @@ public class ReceiptWriter {
         File folder = new File("receipts");
 
         if (!folder.mkdirs()) {
-            folder.mkdirs();
+            if (folder.mkdirs()) {
+                System.out.println("Folder created!");
 
+            } else {
+                System.out.println("ERROR: receipts folder could not be created");
+            }
+        }
+    }
+
+    public void saveReceipt(Order order) {
+        ensureReceiptsFolderExists();
+        String fileName = generateFileName();
+
+        String filePath = "receipts" + File.separator + fileName;
+
+        String orderContent = order.getOrderDetails();
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+
+            bw.write(orderContent);
+            System.out.println("Receipts saved to: " + filePath);
+
+        } catch (IOException e) {
+            System.out.println("ERROR! Could not save file" + e.getMessage());
         }
     }
 }
